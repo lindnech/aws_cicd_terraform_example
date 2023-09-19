be7371ff7201bde43660d48ea5515d24e41af78f
L C <122011721+lindnech@users.noreply.github.com>
2023-09-19 22:41:19 +0200

    Update install-terraform.sh

--- cicd/install-terraform.sh
+++ cicd/install-terraform.sh
@@ -1,4 +1,7 @@
 set -eu
+
+# may be that sudo does not work with some Linux versions, take it out if necessary
+
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
 sudo yum -y install terraform
