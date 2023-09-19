6e1694aa8f7a575ee7ac31838c27ecb558a07e6a
L C <122011721+lindnech@users.noreply.github.com>
2023-09-19 22:36:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 provider "aws" {
   region = "eu-central-1"
-  profile = "codebuild-user"
+  #Man sollte für CodeBuild einen eigenen IAM User erstellen
+  profile = "codebuild-user" #Hier iam benutzer
+}
+
+#Der ganze restliche Aufbau ab hier
+terraform {
+  backend "s3" {
+    bucket  = "terraform-tfstate-speicher-fuer-pipeline-123"
+    key     = "build/terraform.tfstate"
+    region  = "eu-central-1"
+    profile = "build-benutzer"
+  }
 }
